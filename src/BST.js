e2a6c0c1ccbb5d98f88afe0228e110fb5f35f3e3
RhazzXIX <jm21tonio@gmail.com>
2023-02-17 16:02:23 +0800

    Add method deleteRoot and supporting functions
    Created deleteRoot method to delete the specified root.
    Created the getPreviousRoot function to get the previous root of the
    node that will be deleted. The said function helps on the method
    deleteRoot.
    Created the getAppropriateLeaf function to get the suitable root node
    to replace the to be deleted root. This function also healps on the
    method deleteRoot.

--- src/BST.js
+++ src/BST.js
@@ -3,7 +3,10 @@ import { prettyPrint } from "./scripts/loggingConfig";
 import { default as mergeSort2, distinctify } from "./scripts/arrayHandler";
 import makeRandomNumArray from "./scripts/makeRandomArray";
 
-const numArray = [5, 75, 100, 29, 1, 85, 63, 14, 85, 2, 14, 76, 45, 1 , 25 , 66 , 75 , 36 , 9 , 11];
+const numArray = [
+  5, 75, 99, 100, 29, 1, 85, 63, 14, 85, 2, 14, 76, 45, 1, 25, 66, 75, 36, 9,
+  11,
+];
 const testArray = [5, 1, 25];
 
 const Tree = function (array) {
@@ -32,64 +35,116 @@ const Tree = function (array) {
 
   const insertRec = function (value, root) {
     if (root === null) {
-      root = Node(value, null, null)
-      return root
-    };
-    if (value < root.data) {
-      root.left = insertRec(value, root.left)
-    } else if (value > root.data) {
-      root.right = insertRec(value, root.right)
+      root = Node(value, null, null);
+      return root;
     }
-    return root
-  }
+    if (value < root.data) {
+      root.left = insertRec(value, root.left);
+    } else if (value > root.data) {
+      root.right = insertRec(value, root.right);
+    }
+    return root;
+  };
 
   const insert = function (value) {
-    insertRec(value, this)
-  }
-
-  const deleteRec = function(value, root) {
-    console.log(value);
-    if (value === root.data) {
-      root.data = null;
-      return
-    }
-    if (value < root.data) deleteRec(value, root.left)
-    else if (value > root.data) deleteRec(value, root.right);
-  }
-
-  const deleteRoot = function (value) {
-    deleteRec(value, this)
-  }
+    insertRec(value, this);
+  };
 
   const find = function (value, root) {
     if (root === undefined) {
-      return find(value, this)
-    };
+      return find(value, this);
+    }
     if (root === null) {
-      console.log('Data not found')
-      return root
+      console.log("Data not found");
+      return root;
     }
     if (value === root.data) return root;
     if (value < root.data) {
-      return find(value, root.left)
+      return find(value, root.left);
     } else if (value > root.data) {
-      return find(value, root.right)
-    }    
-  }
+      return find(value, root.right);
+    }
+  };
 
+  const getPreviousRoot = function (value, root) {
+    if (root.left.data === value || root.right.data === value) {
+      return root;
+    }
+    if (value < root.data) {
+      return getPreviousRoot(value, root.left);
+    } else if (value > root.data) {
+      return getPreviousRoot(value, root.right);
+    }
+  };
+
+  const getAppropriateLeaf = function (value, root) {
+    if (root.left === null && root.right === null) return root;
+    if (value === root.data) {
+      return getAppropriateLeaf(value, root.right);
+    } else if (value < root.data) {
+      return getAppropriateLeaf(value, root.left);
+    }
+  };
+
+  const deleteRoot = function (value) {
+    console.log(value);
+    const toBeDeleted = find(value, this);
+    if (toBeDeleted === null) return;
+    const previousRoot = getPreviousRoot(value, this);
+    switch (true) {
+      case toBeDeleted.left !== null && toBeDeleted.right === null:
+        if (value < previousRoot.data) {
+          previousRoot.left = toBeDeleted.left;
+        } else if (value > previousRoot.data) {
+          previousRoot.right = toBeDeleted.left;
+        }
+        break;
+      case toBeDeleted.left === null && toBeDeleted.right !== null:
+        if (value < previousRoot.data) {
+          previousRoot.left = toBeDeleted.right;
+        } else if (value > previousRoot.data) {
+          previousRoot.right = toBeDeleted.right;
+        }
+        break;
+      case toBeDeleted.left !== null && toBeDeleted.right !== null:
+        const appropriateLeaf = getAppropriateLeaf(value, toBeDeleted);
+        console.log(appropriateLeaf);
+        deleteRoot.call(this, appropriateLeaf.data);
+        if (value < previousRoot.data) {
+          previousRoot.left = Node(
+            appropriateLeaf.data,
+            toBeDeleted.left,
+            toBeDeleted.right
+          );
+        } else if (value > previousRoot.data) {
+          previousRoot.right = Node(
+            appropriateLeaf.data,
+            toBeDeleted.left,
+            toBeDeleted.right
+          );
+        }
+        break;
+      case toBeDeleted.left === null && toBeDeleted.right === null:
+        if (value < previousRoot.data) {
+          previousRoot.left = null;
+        } else if (value > previousRoot.data) {
+          previousRoot.right = null;
+        }
+        break;
+      default:
+    }
+  };
 
   const balancedBinaryTree = buildBalancedTree(sortedArray);
 
-  return Object.assign(balancedBinaryTree, {insert, deleteRoot, find});
+  return Object.assign(balancedBinaryTree, { insert, deleteRoot, find });
 };
 
-const random1 = makeRandomNumArray(1155)
+const random1 = makeRandomNumArray(352);
 
-const firstTree = Tree(testArray);
-// prettyPrint(firstTree);
-firstTree.insert(3);
-// firstTree.deleteRoot(36);
-// prettyPrint(firstTree);
-console.log(firstTree.find(99));
+const firstTree = Tree(random1);
+prettyPrint(firstTree);
+firstTree.deleteRoot(random1[1]);
+prettyPrint(firstTree);
 
 export default Tree;
